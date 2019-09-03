60e599ffcdd13fbac7e04740a9b88f2b68408c90
Meghan Adkins <meghandadkins@gmail.com>
2019-09-03 09:05:30 -0400

    global styles

---
+++ src/components/MarkdownApp.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledMarkdownApp = styled.div`
+
+`;
+
+function MarkdownApp() {
+  return (
+    <StyledMarkdownApp>
+      
+    </StyledMarkdownApp>
+  )
+}
+
+export default MarkdownApp
